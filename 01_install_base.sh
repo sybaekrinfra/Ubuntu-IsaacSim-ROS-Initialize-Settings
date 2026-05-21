a1afa21f23f99f1c4d52ca85e5b65a1c9161b2bc
SooYoun-Bae-NB\sybae <sybae@krinfra.co.kr>
2026-05-21 09:58:42 +0900

    xubuntu 설정 변경 / banner 삭제

--- 01_install_base.sh
+++ 01_install_base.sh
@@ -1,44 +1,31 @@
-﻿#!/bin/bash
+#!/bin/bash
 set -e
 
-banner() {
-    echo "========================================"
-    echo "$1"
-    echo "========================================"
-}
-
-banner "기본 시스템 및 NVIDIA 드라이버 설치 시작"
-
-echo ""
+echo "기본 시스템 및 NVIDIA 드라이버 설치 시작"
 echo "[1/4] apt 업데이트 및 패키지 업그레이드"
 sudo apt update
 sudo apt upgrade -y
 
-echo ""
 echo "[2/4] 기본 패키지 설치"
 sudo apt install -y \
     build-essential \
     linux-tools-common \
     linux-tools-generic \
-    xubuntu-desktop-minimal \
+    xubuntu-desktop \
     software-properties-common \
     apt-transport-https \
     ca-certificates \
-    sysbanner \
-    figlet \
     gnupg \
     unzip \
     wget \
     curl
 
-echo ""
 echo "[3/4] CPU 성능 모드로 설정"
 bash install/set-cpu-performance.sh
 
-echo ""
 echo "[4/4] NVIDIA 드라이버 설치"
 ubuntu-drivers devices
 sudo ubuntu-drivers install nvidia:580
 
-banner "기본 설치 완료. 이제 재부팅합니다."
+echo "기본 설치 완료. 이제 재부팅합니다."
 sudo reboot
