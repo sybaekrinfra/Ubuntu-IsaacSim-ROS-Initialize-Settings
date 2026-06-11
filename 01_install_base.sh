c0bfe3035831f5e6d9110c9262640e3f3de2f591
SooYoun-Bae-NB\sybae <sybae@krinfra.co.kr>
2026-06-11 10:26:03 +0900

    한글 입력기 추가

--- 01_install_base.sh
+++ 01_install_base.sh
@@ -18,11 +18,11 @@ esac
 echo "Ubuntu codename: ${UBUNTU_CODENAME}, using package: ${XUBUNTU_DESKTOP_PACKAGE}"
 
 echo "기본 시스템 및 NVIDIA 드라이버 설치 시작"
-echo "[1/4] apt 업데이트 및 패키지 업그레이드"
+echo "[1/5] apt 업데이트 및 패키지 업그레이드"
 sudo apt update
 sudo apt upgrade -y
 
-echo "[2/4] 기본 패키지 설치"
+echo "[2/5] 기본 패키지 설치"
 sudo apt install -y \
     build-essential \
     linux-tools-common \
@@ -36,16 +36,19 @@ sudo apt install -y \
     wget \
     curl
 
-echo "[3/4] CPU 성능 모드로 설정"
+echo "[3/5] CPU 성능 모드로 설정"
 if command -v systemd-detect-virt >/dev/null 2>&1 && systemd-detect-virt --vm --quiet; then
     echo "VM 환경이므로 CPU performance 설정을 건너뜁니다."
 else
     bash install/set-cpu-performance.sh
 fi
 
-echo "[4/4] NVIDIA 드라이버 설치"
+echo "[4/5] NVIDIA 드라이버 설치"
 ubuntu-drivers devices
 sudo ubuntu-drivers install nvidia:580
+
+echo "[5/5] 한글 입력기 ibus-hangul 설치"
+sudo apt install ibus-hangul
 
 echo "기본 설치 완료. 이제 재부팅합니다."
 sudo reboot
